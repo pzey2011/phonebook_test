e961fa2fd195df718ded7d740a482392ed91bb16
pzey2011 <peymanzeynali@yahoo.com>
2017-08-06 14:08:29 +0430

    edit UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,13 @@
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-login">
                   <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-6">
-                            <a href="#" class="active" id="login-form-link">Login</a>
-                        </div>
-                        <div class="col-xs-6">
-                            <a href="#" id="register-form-link">Register</a>
-                        </div>
-                    </div>
+                        
+
+
+                            <a>Login</a>
+
+
+
                     <hr>
                 </div>
                 <div class="panel-body">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,13 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+            <div class="panel panel-login">
+                <div class="panel-heading">
+                    <a>Register</a>
+                    <hr>
+                </div>
+
+                </div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
